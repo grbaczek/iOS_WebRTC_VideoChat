2ea07e3321dc07436ec1ebfe5f41a6733ecebfd0
Grzegorz Baczek <gb@cardlay.com>
2022-11-14 06:58:48 +0100

    initial commit

---
+++ VideoChat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VideoChat
+//
+//  Created by Grzegorz Baczek on 14/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoChat/VideoChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoChatApp.swift
+//  VideoChat
+//
+//  Created by Grzegorz Baczek on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct VideoChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
